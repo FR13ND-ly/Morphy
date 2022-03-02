7f52dd980f24a618e04e2567d7d985383a3689f6
Alan <57838113+FR13ND-ly@users.noreply.github.com>
2022-03-02 09:31:46 +0200

    =u

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/frontend'));
+app.use(express.static('./dist/morphy'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/frontend/'}),
+    res.sendFile('index.html', {root: 'dist/morphy/'}),
 );
 
 // Start the app by listening on the default Heroku port
